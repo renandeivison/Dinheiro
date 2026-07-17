1fc97e63e427acc5969096ccd4b218721f7fc325
renandeivison <renandeivison@gmail.com>
2026-07-17 11:45:54 -0300

    Update CACHE_VERSION to 'v1.1'

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  * pra renderizar a tipografia quando offline).
  */
 
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v1.1';
 const APP_SHELL_CACHE = `mt-app-shell-${CACHE_VERSION}`;
 const FONTS_CACHE = `mt-fonts-${CACHE_VERSION}`;
 
