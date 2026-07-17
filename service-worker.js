aaf82f80dbcd0b91f4a96162220ad4c1de80fbca
renandeivison <renandeivison@gmail.com>
2026-07-17 12:11:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  * pra renderizar a tipografia quando offline).
  */
 
-const CACHE_VERSION = 'v1.1';
+const CACHE_VERSION = 'v1.2';
 const APP_SHELL_CACHE = `mt-app-shell-${CACHE_VERSION}`;
 const FONTS_CACHE = `mt-fonts-${CACHE_VERSION}`;
 
